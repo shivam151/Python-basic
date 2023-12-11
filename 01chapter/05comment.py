5748333dc6c113292ef5c66e4886a5a30543a090
shivam151 <shivamkati45@gmail.com>
2023-12-11 20:37:36 +0530

    third

--- 01chapter/05comment.py
+++ 01chapter/05comment.py
@@ -1,4 +1,11 @@
+# Comment and Escape Sequence Charatrers
 
+#This is not execute
+
+# print("This doesnt "execute")
+# print("This will \" execute") 
+
+print  ("Hello my name is \n shivam katiyar ")
 
 #It will execute a block of code if a specified condition is true.
 #If the condition is false then it will execute another block of code.
---
+++ 01chapter/07VariablesDataTypes.py
@@ -0,0 +1,21 @@
+
+a = 1
+print(a)
+#String
+b="shivam katiyar"
+print(type(b))
+print(b)
+d=None
+print(type(d))
+
+List=[1,2,3,3,["shivam","katiyar"]]
+print(List)
+print(type(List))
+
+tuplue=(1,2,3,4,5,6,7,8,1)
+print(tuplue)
+print(type(tuplue))
+
+dic={"name":"shivam","age":21 ,"drinving":True,"drinving":True}
+print(dic)
+print(type(dic))
